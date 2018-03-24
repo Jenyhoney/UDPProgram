d47030e0d216a74604bf11b848b23d935f0111e6
treesarose <36188557+treesarose@users.noreply.github.com>
2018-03-24 06:53:46 +0000

    Update UDPServer.java

--- UDPServer.java
+++ UDPServer.java
@@ -3,8 +3,7 @@ import java.net.*;
 class UDPServer
 {
    public static void main(String args[]) throws Exception
-      {
-         System.out.println("Client Saying");
+   {
          DatagramSocket serverSocket = new DatagramSocket(9876);
             byte[] receiveData = new byte[1024];
             byte[] sendData = new byte[1024];
