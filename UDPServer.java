87376594cf276e7b876b230fda33d5a2bb5d84a5
Jenyhoney <36188597+Jenyhoney@users.noreply.github.com>
2018-03-24 06:50:15 +0000

    Update UDPServer.java

--- UDPServer.java
+++ UDPServer.java
@@ -4,6 +4,7 @@ class UDPServer
 {
    public static void main(String args[]) throws Exception
       {
+         System.out.println("Client Saying");
          DatagramSocket serverSocket = new DatagramSocket(9876);
             byte[] receiveData = new byte[1024];
             byte[] sendData = new byte[1024];
