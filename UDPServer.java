adf7dafd0f3be06ba61af7f4b5a0d4cbf1172b9d
treesarose <36188557+treesarose@users.noreply.github.com>
2018-04-04 10:47:50 +0530

    Update UDPServer.java

--- UDPServer.java
+++ UDPServer.java
@@ -6,8 +6,7 @@ class UDPServer{
          DatagramSocket serverSocket = new DatagramSocket(9876);
             byte[] receiveData = new byte[1024];
             byte[] sendData = new byte[1024];
-            while(true)
-               {
+            while(true){
                   DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                   serverSocket.receive(receivePacket);
                   String sentence = new String( receivePacket.getData());
