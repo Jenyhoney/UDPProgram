b8a02462dcefb4a023745adf7a1e4be5a882acdc
treesarose <36188557+treesarose@users.noreply.github.com>
2018-04-04 10:45:09 +0530

    Update UDPClient.java

--- UDPClient.java
+++ UDPClient.java
@@ -1,8 +1,7 @@
 import java.io.*;
 import java.net.*;
 class UDPClient{
-   public static void main(String args[]) throws Exception
-   {
+   public static void main(String args[]) throws Exception{
       BufferedReader inFromUser =
       new BufferedReader(new InputStreamReader(System.in));
       DatagramSocket clientSocket = new DatagramSocket();
