b050a9ea02d0490ba325142c0a294dfb8db559af
treesarose <36188557+treesarose@users.noreply.github.com>
2018-04-04 10:49:13 +0530

    Update UDPServer.java

--- UDPServer.java
+++ UDPServer.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.net.*;
- 
 class UDPServer{
    public static void main(String args[]) throws Exception{
          DatagramSocket serverSocket = new DatagramSocket(9876);
